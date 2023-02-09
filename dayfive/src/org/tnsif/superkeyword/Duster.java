9da0e0d67e29c38f0591fa39c9611b3808c8077f
Shital <shitalmoule1234@gmail.com>
2023-02-09 15:29:59 +0100

    First Commit

---
+++ dayfive/src/org/tnsif/superkeyword/Duster.java
@@ -0,0 +1,13 @@
+package org.tnsif.superkeyword;
+
+//child class
+public class Duster extends Renault {
+	private int speed = 120;
+	
+	public void print()
+	{
+		super.print();  //it will call to parent class method
+		System.out.println("The speed of Duster is: " +speed+ "km/hr");
+	}
+	
+}
---
+++ dayfive/src/org/tnsif/superkeyword/Renault.java
@@ -0,0 +1,13 @@
+package org.tnsif.superkeyword;
+
+//parent class
+public class Renault {
+	
+	private String engine="Petrol";
+	
+	public void print()
+	{
+		System.out.println("Engine required: "+ engine);
+	}
+
+}
