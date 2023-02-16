dbffb59148f6215d18fdf1e34ccd30587d882344
Shital <shitalmoule1234@gmail.com>
2023-02-16 23:38:32 +0530

    First Commit

---
+++ dayten/src/org/tnsif/threadlifecycle/Sender.java
@@ -0,0 +1,5 @@
+package org.tnsif.threadlifecycle;
+
+public class Sender {
+
+}
---
+++ dayten/src/org/tnsif/threadlifecycle/ThreadLifeCycleDemo.java
@@ -0,0 +1,16 @@
+package org.tnsif.threadlifecycle;
+//driver class
+//program to demonstrate on thread life cycle
+public class ThreadLifeCycleDemo {
+
+	public static void main(String[] args) {
+		Tech t = new Tech();
+		SoftSkill s = new SoftSkill();
+		t.start();
+		t.yield();	//move control to the another state
+		
+		s.start();
+
+	}
+
+}
