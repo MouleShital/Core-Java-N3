0393da727ed900d00b4666826a3f1a69013e7818
Shital <shitalmoule1234@gmail.com>
2023-02-16 23:19:42 +0530

    First Commit

---
+++ daynine/src/org/tnsif/customexception/LoginCredential.java
@@ -0,0 +1,21 @@
+package org.tnsif.customexception;
+
+public class LoginCredential extends Exception{
+	
+	//data members
+	private String str;
+	
+	//parameterized constructor
+	public LoginCredential(String str) {
+		super();
+		this.str = str;
+	}
+
+	@Override
+	public String toString() {
+		return "LoginCredential [str=" + str + "]";
+	}
+	
+	
+
+}
