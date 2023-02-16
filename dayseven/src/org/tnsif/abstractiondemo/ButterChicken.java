d360f55767db3a8f0e7f054036cc85c5c74f2bc9
Shital <shitalmoule1234@gmail.com>
2023-02-16 23:00:02 +0530

    First Commit

---
+++ dayseven/src/org/tnsif/abstractiondemo/ButterChicken.java
@@ -0,0 +1,20 @@
+package org.tnsif.abstractiondemo;
+
+//implementable class
+public class ButterChicken extends Menu{
+	
+	//implement all the abstract method of abstract class
+	@Override
+	void receipe() 
+	{
+		System.out.println("Wash and cook a Chicken");
+	}
+
+	@Override
+	void foodType() 
+	{
+		System.out.println("Non Veg");
+	}
+	
+	
+}
